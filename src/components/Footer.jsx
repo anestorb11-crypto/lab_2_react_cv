07eef193a0a7e45c1e52d301f837e54b1911a7a3
Andrii Nestor <anestorb11@gmail.com>
2026-03-09 11:30:12 +0200

    Recreated CV using React components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '20px', marginTop: '20px', borderTop: '1px solid #ccc' }}>
+      <p>© 2026 Нестор Андрій. Всі права захищені.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Нестор Андрій</h1>
+      <p>Системний адміністратор (Junior SysAdmin) / Молодший спеціаліст з кібербезпеки</p>
+      <p>Phone: +380997351660 | Email: andrii.neestor@gmail.com</p>
+      <p>Address: Lviv, Ukraine</p>
+      <p>Portfolio: <a href="https://www.credly.com/users/andrii-nestor.d33819a2/badges#credly" target="_blank">credly.com</a></p>
+    </header>
+  );
+}
+
+export default Header;
